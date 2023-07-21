df36a2c7d5f3a233542ce0a9879dc316ca092d90
Jorge Carbonell <jcnavio@gmail.com>
2023-07-21 18:35:31 +0200

    Added Dockerfile to coinmarketcap

--- coinmarketcap/config.py
+++ coinmarketcap/config.py
@@ -9,3 +9,7 @@ URL_HISTORICAL = "/".join([os.getenv(path) for path in ["URL", "VERSION", "ENDPO
 URL_VERSION = os.getenv("VERSION")
 API_KEY = os.getenv('API_KEY')
 ENDPOINT_TIMEOUT = int(os.getenv("ENDPOINT_TIMEOUT"))
+
+WEB_HOST = os.getenv("WEB_HOST", "0.0.0.0")
+WEB_PORT = int(os.getenv("WEB_PORT", 8081))
+DEBUG = os.getenv("DEBUG", "1")
--- coinmarketcap/main.py
+++ coinmarketcap/main.py
@@ -1,6 +1,14 @@
+import os
+
 import uvicorn
+import logging
 
 from app import coinmarketApp  # noqa
+from config import WEB_HOST, WEB_PORT, DEBUG
+
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    uvicorn.run("main:coinmarketApp", reload=True)
+    for k, v in os.environ.items():
+        logging.info(f'{k}={v}')
+    uvicorn.run("main:coinmarketApp", reload=bool(DEBUG), host=WEB_HOST, port=WEB_PORT)
