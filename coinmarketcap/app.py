2469e50fb73e0e11fbc4aef317fce7dd6b0ace01
Jorge Carbonell <jcnavio@gmail.com>
2023-07-22 12:53:59 +0200

    Implemented CoinMarketCap API

--- coinmarketcap/app.py
+++ coinmarketcap/app.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-import datetime
 from typing import Dict, Any
 
 import logging
@@ -11,31 +10,47 @@ from config import API_KEY, URL_LATEST, URL_HISTORICAL, URL_VERSION, ENDPOINT_TI
 
 
 class AppV1:
-    def __init__(self, limit: int = 5000, datetime: datetime = datetime.datetime.now()) -> None:
+    def __init__(self) -> None:
         self.api = FastAPI(debug=True)
-        self.api.get("/latest")(self.get_latest)
+        self.api.api_route("/latest")(self.get_latest)
+        self.api.api_route("/historical")(self.get_historical)
 
-        self.parameters: Dict = {
-            'start': '1',
-            'limit': limit,
-            'convert': 'USD'
-        }
+        self.parameters: Dict = {}
 
         self.session = Session()
-        self.session.headers.update({
-            'Accepts': 'application/json',
-            'X-CMC_PRO_API_KEY': API_KEY,
-        })
+        self.session.headers.update(
+            {
+                "Accepts": "application/json",
+                "Accept-Encoding": "deflate, gzip",
+                "X-CMC_PRO_API_KEY": API_KEY,
+            }
+        )
 
-    def get_latest(self):
-        return self.get_coinmarketcap(URL_LATEST)
+    def get_latest(self, limit: str):
+        return self.get_coinmarketcap(URL_LATEST, {"limit": limit})
 
-    def get_coinmarketcap(self, url: str):
+    def get_historical(self, limit: int, date: str):
+        return self.get_coinmarketcap(URL_HISTORICAL, {"limit": limit, "date": date})
+
+    def get_coinmarketcap(self, url: str, additional_query_params: Dict = None):
         try:
-            response = self.session.get(url, params=self.parameters, timeout=ENDPOINT_TIMEOUT)
-            return json.loads(response.text)
+            additional_query_params = additional_query_params or {}
+            # fmt: off
+            response = self.session.get(url, params={**self.parameters, **additional_query_params}, timeout=ENDPOINT_TIMEOUT)
+            return self.clean(response.text)
+            # fmt: on
         except (ConnectionError, Timeout, TooManyRedirects) as error:
             logging.error(error)
+
+    def clean(self, response: str) -> Dict:
+        response_json = json.loads(response)
+        if "data" not in response_json:
+            return response_json
+
+        return {
+            item["symbol"]: item["quote"]["USD"]["price"]
+            for item in response_json["data"]
+        }
 
 
 class AppV2:
@@ -43,7 +58,14 @@ class AppV2:
         raise NotImplementedError
 
 
-coinmarketApp: Dict[str, Any] = {
+# fmt: off
+coinmarketApi: Dict[str, Any] = {
     "v1": AppV1,
     "v2": AppV2
 }.get(URL_VERSION)().api
+
+coinmarketApp: Dict[str, Any] = {
+    "v1": AppV1,
+    "v2": AppV2
+}.get(URL_VERSION)
+# fmt: on
--- coinmarketcap/main.py
+++ coinmarketcap/main.py
@@ -1,14 +1,10 @@
-import os
-
 import uvicorn
 import logging
 
-from app import coinmarketApp  # noqa
+from app import coinmarketApi  # noqa
 from config import WEB_HOST, WEB_PORT, DEBUG
 
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    for k, v in os.environ.items():
-        logging.info(f'{k}={v}')
-    uvicorn.run("main:coinmarketApp", reload=bool(DEBUG), host=WEB_HOST, port=WEB_PORT)
+    uvicorn.run("main:coinmarketApi", reload=bool(DEBUG), host=WEB_HOST, port=WEB_PORT)
