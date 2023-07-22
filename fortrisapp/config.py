20a02591a22a817bd3576f1ef0f85fd75c8c38ec
Jorge Carbonell <jcnavio@gmail.com>
2023-07-22 21:02:42 +0200

    Added initial files for the main fortris app

---
+++ fortrisapp/config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENDPOINT_TIMEOUT = int(os.getenv("ENDPOINT_TIMEOUT"))
+
+WEB_HOST = os.getenv("WEB_HOST", "0.0.0.0")
+WEB_PORT = int(os.getenv("WEB_PORT", 8080))
+DEBUG = os.getenv("DEBUG", "1")
