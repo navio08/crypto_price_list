ae87f3005c50aab1a65dd81ebc7b3e040cc63a3f
Jorge Carbonell <jcnavio@gmail.com>
2023-07-22 21:03:44 +0200

    Implemented main app  Fortris

---
+++ fortrisapp/app.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from requests import Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from typing import Dict
+
+import logging
+import concurrent.futures
+from uuid import uuid4
+
+URL_COINMARKET = "http://localhost:8081/latest"
+URL_CRYPTORANK = "http://localhost:8082/ranklatest"
+
+
+class App:
+    def __init__(self) -> None:
+        self.api = FastAPI(debug=True)
+        self.api.api_route("/")(self.get_data)
+
+        self.parameters: Dict = {}
+        self.headers: Dict = {
+            "Accepts": "application/json",
+            "Accept-Encoding": "deflate, gzip",
+        }
+
+    def get_data(self, limit: int):
+        try:
+            return self.fetch_data_multithreaded(limit)
+        except (ConnectionError, Timeout, TooManyRedirects) as error:
+            logging.error(error)
+        except Exception as error:
+            logging.error(f"Unknown error. Try again later: {str(error)}")
+
+    def fetch_data_multithreaded(self, limit):
+        coinmarketUUID, cryptorankUUID = str(uuid4().hex), str(uuid4().hex)
+        _my_futures = {coinmarketUUID: None, cryptorankUUID: None}
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.get_coinmarketcap, limit=limit, cid=coinmarketUUID),
+                executor.submit(self.get_cryptorank, limit=limit, cid=cryptorankUUID)
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                _my_futures[self.get_uuid(future.result())] = future.result().text
+
+        print(_my_futures)
+        return _my_futures
+
+    def get_coinmarketcap(self, limit: int, cid: str):
+        return self.fetch(URL_COINMARKET, limit, cid)
+
+    def get_cryptorank(self, limit: int, cid: str):
+        return self.fetch(URL_CRYPTORANK, limit, cid)
+
+    def fetch(self, url: str, limit: int, cid: str):
+        session = Session()
+        session.headers.update(self.headers)
+        session.headers.update({"cid": cid})
+        return session.get(url=url, params={"limit": limit})
+
+    def get_uuid(self, future):
+        return future.headers["cid"]
+
+
+fortrisApi = App().api
+fortrisApp = App()
---
+++ fortrisapp/main.py
@@ -0,0 +1,10 @@
+import uvicorn
+import logging
+
+from app import fortrisApi  # noqa
+from config import WEB_HOST, WEB_PORT, DEBUG
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    uvicorn.run("main:fortrisApi", reload=bool(DEBUG), host=WEB_HOST, port=WEB_PORT)
